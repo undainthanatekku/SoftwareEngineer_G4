38edf8441439ae708f5913cf0aecd61033a1956a
Unda Inthanate <unda.i@kkumail.com>
2024-02-28 23:15:10 +0700

    add prestashop file

---
+++ Deliverables Sprint#3/PrestaShop-develop/tests/UI/pages/FO/hummingbird/cart/index.ts
@@ -0,0 +1,21 @@
+// Import FO pages
+import {CartPage} from '@pages/FO/classic/cart/index';
+
+/**
+ * Cart page, contains functions that can be used on the page
+ * @class
+ * @extends FOBasePage
+ */
+class Cart extends CartPage {
+  /**
+   * @constructs
+   */
+  constructor() {
+    super('hummingbird');
+
+    this.proceedToCheckoutButton = '#wrapper div.cart-summary div.checkout a.btn';
+    this.noItemsInYourCartSpan = '#content-wrapper div.cart-overview p';
+  }
+}
+
+export default new Cart();
---
+++ Deliverables Sprint#3/PrestaShop-develop/tests/UI/pages/FO/hummingbird/home/index.ts
@@ -0,0 +1,70 @@
+import {Page} from 'playwright';
+// Import FO Pages
+import {HomePage} from '@pages/FO/classic/home';
+
+/**
+ * Home page, contains functions that can be used on the page
+ * @class
+ * @extends HomePage
+ */
+class Home extends HomePage {
+  private readonly productAddToWishlist: (number: number) => string;
+
+  private readonly wishlistModal: string;
+
+  private readonly wishlistModalTitle: string;
+
+  private readonly addToCartIcon: (number: number) => string;
+
+  /**
+   * @constructs
+   * Setting up texts and selectors to use on home page
+   */
+  constructor() {
+    super('hummingbird');
+
+    // Products list
+    this.productArticle = (number: number) => `#content section.featured-products div.container div article:nth-child(${number})`;
+    this.addToCartIcon = (number: number) => `${this.productArticle(number)} button[data-button-action='add-to-cart']`;
+    this.productAddToWishlist = (number: number) => `${this.productArticle(number)} button.wishlist-button-add`;
+
+    // Block Cart Modal
+    this.cartModalCheckoutLink = `${this.blockCartModalDiv} div.cart-footer-actions a`;
+
+    // Wishlist modal
+    this.wishlistModal = '.wishlist-add-to .wishlist-modal.show';
+    this.wishlistModalTitle = '.wishlist-modal.show p.modal-title';
+
+    // Newsletter form
+    this.newsletterFormField = '#footer div.email-subscription__content__right input[name="email"]';
+    this.newsletterSubmitButton = '.email-subscription__content__inputs [name="submitNewsletter"][value="Subscribe"]';
+    this.subscriptionAlertMessage = '#footer div.email-subscription__content__infos p.alert';
+  }
+
+  /**
+   * Add a product (based on its index) to the first wishlist
+   * @param page {Page} Browser tab
+   * @param idxProduct {number} Id of product
+   * @returns Promise<string>
+   */
+  async clickOnAddToWishListLink(page: Page, idxProduct: number): Promise<string> {
+    // Click on the heart
+    await page.locator(this.productAddToWishlist(idxProduct)).click();
+    // Wait for the modal
+    await this.elementVisible(page, this.wishlistModal, 2000);
+
+    return this.getTextContent(page, this.wishlistModalTitle);
+  }
+
+  /**
+   * Is add to cart button visible
+   * @param page {Page} Browser tab
+   * @param id {number} Id of product
+   * @returns Promise<boolean>
+   */
+  async isAddToCartButtonVisible(page: Page, id: number = 1): Promise<boolean> {
+    return this.elementVisible(page, this.addToCartIcon(id), 1000);
+  }
+}
+
+export default new Home();
